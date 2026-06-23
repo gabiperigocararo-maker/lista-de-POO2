f3ff074aed71c2811517da5c31e7f99eb8b1bf06
gabiperigocararo <gabiperigocararo@gmail.com>
2026-06-22 22:35:07 -0400

    Primeiro commit

--- attSlides1.js
+++ attSlides1.js
@@ -1,9 +1,26 @@
 class ValidarCadastro {
-    constructor(nome) {
+ValidarSenha(nome) {
         if (typeof nome !== "string" || nome.length < 3) {
             throw new Error ("o nome deve possuir pelo menos tres caracteres");
             
         }
-        return true
+        return true;
+    }
+
+
+    ValidarSenha(senha) {
+        if (typeof senha !== "string" || nome.length < 8) {
+            throw new Error ("a senha deve conter apenas numeros e ter pelo menos 8 caracteres")
+        }
+        return true;
+    }
+
+    ValidarEmail(email) {
+         const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+         if (!regex.test(email)) {
+            throw new Error ("E-mail invalido!");
+         }
+        return true;
     }
 }
